5ea029f306af7ccd6455f1efb49193fdf7cf39ca
QreLLinG <supere98@yandex.ru>
2026-04-06 13:28:06 +0800

    Update var4.pl

--- lab1/var4/var4.pl
+++ lab1/var4/var4.pl
@@ -1,15 +1,6 @@
-mary_loves(peaches).
-mary_loves(corn).
-mary_loves(apples).
-
-beth_loves(X) :-
-    mary_loves(X),
-    fruit(X),
-    color(X, red).
-
-beth_loves(X) :-
-    mary_loves(X),
-    X = corn.
+loves(mary, peaches).
+loves(mary, corn).
+loves(mary, apples).
 
 fruit(peaches).
 fruit(apples).
@@ -18,3 +9,31 @@ color(peaches, yellow).
 color(apples, red).
 color(apples, yellow).
 color(oranges, orange).
+loves(beth, X) :- 
+    loves(mary, X), 
+    fruit(X), 
+    color(X, red).
+
+loves(beth, X) :- 
+    loves(mary, X), 
+    X = corn.
+
+% ----- ЗАПРОСЫ -----
+
+% 1. Что любит Бет?
+?- loves(beth, What).
+What = apples ;
+What = corn.
+
+% 2. Любит ли Мэри кукурузу?
+?- loves(mary, corn).
+true.
+
+% 3. Какие фрукты известны?
+?- fruit(X).
+X = peaches ;
+X = apples.
+
+% 4. Только красные фрукты (исправленный запрос)
+?- loves(mary, Fruit), loves(beth, Fruit), fruit(Fruit), color(Fruit, red).
+Fruit = apples.
